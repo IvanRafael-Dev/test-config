235304b2dfb83e408c17dd0707602f23f86070f8
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2023-01-20 13:33:42 -0300

    test: userController deve retonar status 400 quando password não é informado

--- test/integration/User.test.ts
+++ test/integration/User.test.ts
@@ -3,15 +3,19 @@
 import supertest from 'supertest'
 import app from '../../src/api/app'
 
-describe('UserController', () => {
+describe('User Controller', () => {
   describe('#add', () => {
-    describe('quando o email nao é informado', () => {
+    describe('quando o campo email não é informado', () => {
       it('deve retornar um status 400', async () => {
-        const httpResponse = await supertest(app)
-          .post('/users')
-          .send({})
-        expect(httpResponse.status).toEqual(400)
-        expect(httpResponse.body).toStrictEqual({ error: 'email is required' })
+        const httpRequest = await supertest(app).post('/users').send({ password: 'any_password' })
+        expect(httpRequest.statusCode).toEqual(400)
+      })
+    })
+
+    describe('quando o campo password não é informado', () => {
+      it('deve retornar um status 400', async () => {
+        const httpRequest = await supertest(app).post('/users').send({ email: 'any_email' })
+        expect(httpRequest.statusCode).toEqual(400)
       })
     })
   })
